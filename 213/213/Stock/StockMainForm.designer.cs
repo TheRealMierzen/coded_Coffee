3ad726a4f4e4aebb328bea94d5c30ab00ff2622f
Marco <marcotaljaard@gmail.com>
2016-10-09 00:39:32 +0200

    Merge Q

--- 213/213/Stock/StockMainForm.designer.cs
+++ 213/213/Stock/StockMainForm.designer.cs
@@ -28,7 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
             this.cmbSelectStockAction = new System.Windows.Forms.ComboBox();
             this.btnBackMain = new System.Windows.Forms.Button();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
@@ -43,7 +42,7 @@
             this.label1 = new System.Windows.Forms.Label();
             this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
             this.lblActionSelect = new System.Windows.Forms.Label();
-            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.toolTip1 = new System.Windows.Forms.ToolTip();
             this.tableLayoutPanel1.SuspendLayout();
             this.tableLayoutPanel2.SuspendLayout();
             this.tableLayoutPanel3.SuspendLayout();
@@ -60,9 +59,9 @@
             "Update Stock",
             "Transfer Stock",
             "Remove Stock"});
-            this.cmbSelectStockAction.Location = new System.Drawing.Point(60, 3);
+            this.cmbSelectStockAction.Location = new System.Drawing.Point(59, 3);
             this.cmbSelectStockAction.Name = "cmbSelectStockAction";
-            this.cmbSelectStockAction.Size = new System.Drawing.Size(193, 21);
+            this.cmbSelectStockAction.Size = new System.Drawing.Size(192, 21);
             this.cmbSelectStockAction.TabIndex = 1;
             this.toolTip1.SetToolTip(this.cmbSelectStockAction, "Select an action to visit that window");
             this.cmbSelectStockAction.SelectedIndexChanged += new System.EventHandler(this.cmbSelectStockAction_SelectedIndexChanged);
@@ -71,9 +70,9 @@
             // btnBackMain
             // 
             this.btnBackMain.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.btnBackMain.Location = new System.Drawing.Point(259, 3);
+            this.btnBackMain.Location = new System.Drawing.Point(257, 3);
             this.btnBackMain.Name = "btnBackMain";
-            this.btnBackMain.Size = new System.Drawing.Size(92, 35);
+            this.btnBackMain.Size = new System.Drawing.Size(94, 35);
             this.btnBackMain.TabIndex = 4;
             this.btnBackMain.Text = "Back";
             this.toolTip1.SetToolTip(this.btnBackMain, "Click to close this form and go back to main form");
@@ -107,9 +106,9 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.label2.Location = new System.Drawing.Point(423, 105);
+            this.label2.Location = new System.Drawing.Point(423, 103);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(354, 13);
+            this.label2.Size = new System.Drawing.Size(354, 15);
             this.label2.TabIndex = 24;
             this.label2.Text = "Stock Status from Today";
             // 
@@ -140,7 +139,7 @@
             this.tableLayoutPanel2.ColumnCount = 3;
             this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 22.47191F));
             this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 77.52809F));
-            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 95F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 97F));
             this.tableLayoutPanel2.Controls.Add(this.btnCompare, 2, 0);
             this.tableLayoutPanel2.Controls.Add(this.lblStockTakeItemID, 0, 0);
             this.tableLayoutPanel2.Controls.Add(this.txtStockTakeItemID, 1, 0);
@@ -158,9 +157,9 @@
             // btnCompare
             // 
             this.btnCompare.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.btnCompare.Location = new System.Drawing.Point(261, 3);
+            this.btnCompare.Location = new System.Drawing.Point(259, 3);
             this.btnCompare.Name = "btnCompare";
-            this.btnCompare.Size = new System.Drawing.Size(90, 26);
+            this.btnCompare.Size = new System.Drawing.Size(92, 26);
             this.btnCompare.TabIndex = 16;
             this.btnCompare.Text = "Compare/Add";
             this.toolTip1.SetToolTip(this.btnCompare, "Click to add item to check list");
@@ -173,23 +172,23 @@
             this.lblStockTakeItemID.Dock = System.Windows.Forms.DockStyle.Fill;
             this.lblStockTakeItemID.Location = new System.Drawing.Point(3, 0);
             this.lblStockTakeItemID.Name = "lblStockTakeItemID";
-            this.lblStockTakeItemID.Size = new System.Drawing.Size(52, 32);
+            this.lblStockTakeItemID.Size = new System.Drawing.Size(51, 32);
             this.lblStockTakeItemID.TabIndex = 14;
             this.lblStockTakeItemID.Text = "Item ID";
             // 
             // txtStockTakeItemID
             // 
             this.txtStockTakeItemID.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.txtStockTakeItemID.Location = new System.Drawing.Point(61, 3);
+            this.txtStockTakeItemID.Location = new System.Drawing.Point(60, 3);
             this.txtStockTakeItemID.Name = "txtStockTakeItemID";
             this.txtStockTakeItemID.ShortcutsEnabled = false;
-            this.txtStockTakeItemID.Size = new System.Drawing.Size(194, 20);
+            this.txtStockTakeItemID.Size = new System.Drawing.Size(193, 20);
             this.txtStockTakeItemID.TabIndex = 15;
             this.toolTip1.SetToolTip(this.txtStockTakeItemID, "Scan in the ID you have to let the system know you have that item");
             // 
             // btnCompleteStockTake
             // 
-            this.btnCompleteStockTake.Location = new System.Drawing.Point(261, 35);
+            this.btnCompleteStockTake.Location = new System.Drawing.Point(259, 35);
             this.btnCompleteStockTake.Name = "btnCompleteStockTake";
             this.btnCompleteStockTake.Size = new System.Drawing.Size(85, 26);
             this.btnCompleteStockTake.TabIndex = 20;
@@ -204,7 +203,7 @@
             this.label1.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.label1.Location = new System.Drawing.Point(3, 32);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(52, 33);
+            this.label1.Size = new System.Drawing.Size(51, 33);
             this.label1.TabIndex = 21;
             this.label1.Text = "Stock Items Recorded";
             // 
@@ -213,7 +212,7 @@
             this.tableLayoutPanel3.ColumnCount = 3;
             this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 22.26415F));
             this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 77.73585F));
-            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 97F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 99F));
             this.tableLayoutPanel3.Controls.Add(this.lblActionSelect, 0, 0);
             this.tableLayoutPanel3.Controls.Add(this.btnBackMain, 2, 0);
             this.tableLayoutPanel3.Controls.Add(this.cmbSelectStockAction, 1, 0);
@@ -231,7 +230,7 @@
             this.lblActionSelect.Dock = System.Windows.Forms.DockStyle.Fill;
             this.lblActionSelect.Location = new System.Drawing.Point(3, 0);
             this.lblActionSelect.Name = "lblActionSelect";
-            this.lblActionSelect.Size = new System.Drawing.Size(51, 41);
+            this.lblActionSelect.Size = new System.Drawing.Size(50, 41);
             this.lblActionSelect.TabIndex = 0;
             this.lblActionSelect.Text = "Select Action";
             // 
